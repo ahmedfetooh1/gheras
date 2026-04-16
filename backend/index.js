844889259a0b322542ce027b1a3dd7d87af61dab
Ahmed Ramadan <103392399+A7med267@users.noreply.github.com>
2026-04-16 18:24:10 +0200

    Configure backend for production on Railway (update start script)

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv'); 
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(passport.initialize()); 
+app.use(passport.initialize());
 
 
 // main route or project 
